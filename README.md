eac23fa94f106c04505b3822f05d9f2673ad3cc2
CUBEHARD <cubehardbusiness@gmail.com>
2024-09-14 23:20:40 +0200

    Initial powershell script commit and simple README.

---
+++ README.md
@@ -0,0 +1,8 @@
+How to run:
+ - Download fabric loader (1.21.1 version) preferably with auto creation of profile.
+ - Run minecraft with created fabric profile.
+ - Close minecraft.
+ - Go to `%appdata%/.minecraft` and open `mods` folder.
+ - Clone this repository into mods folder.
+ - Right click to `mods-downloader` and select `Run with powershell`.
+ - Done.
---
+++ mods-downloader.ps1
@@ -0,0 +1,28 @@
+# File with links
+$linksFile = "links.txt"
+$successCount = 0
+$totalCount = 0
+
+# Reading links from the source file
+Get-Content $linksFile | ForEach-Object {
+    $line = $_
+    $totalCount++
+
+    # Getting URL from line
+    if ($line -match '"(.*?)"') {
+        $url = $matches[1]
+        $fileName = [System.IO.Path]::GetFileName($url)
+
+        # Downloading file using Invoke-WebRequest
+        try {
+            Invoke-WebRequest -Uri $url -OutFile $fileName
+            Write-Host "Downloaded: $fileName"
+            $successCount++
+        } catch {
+            Write-Host "An error occurred during downloading: $fileName"
+        }
+    }
+}
+
+# Printing status after downloading sequence
+Write-Host "Successfully downloaded $successCount z $totalCount files."
